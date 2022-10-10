20fb6b0f9e7fdbce7fab34b1f45d8d52d546d3bd
MJackson22-bit <jm5444859@gmail.com>
2022-10-10 02:55:03 -0600

    Redesign logic get songs

--- app/src/main/java/com/example/mp3x/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/mp3x/ui/main/view/MainActivity.kt
@@ -1,123 +1,69 @@
 package com.example.mp3x.ui.main.view
 
 import android.Manifest
-import android.content.ContentUris
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.AudioManager
-import android.media.MediaMetadataRetriever
-import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.mp3x.adapter.MusicAdapter
+import androidx.core.view.isVisible
 import com.example.mp3x.databinding.ActivityMainBinding
 import com.example.mp3x.ui.main.viewmodel.MainViewModel
-import com.example.mp3x.model.Music
-import com.example.mp3x.provider.ProviderMusic
 
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: MusicAdapter
 
-    private var mediaPlayer = MediaPlayer()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         runtimePermission()
-        initRecycler()
         setObservers()
+        setListener()
     }
 
-    private fun setObservers() {
-
-    }
-
-    private fun initRecycler() {
-        adapter = MusicAdapter(
-            listMusic = ProviderMusic.listMusic,
-            onClickListener = {music -> onItemSelected(music) }
-        )
-        val manager = LinearLayoutManager(this)
-        binding.recyclerView.layoutManager = manager
-        binding.recyclerView.adapter = adapter
-    }
-
-    private fun onItemSelected(item: Music) {
-        val uri = Uri.parse(item.uri.toString())
-        if(mediaPlayer.isPlaying){
-            mediaPlayer.release()
+    private fun setListener() {
+        binding.layoutPermissionNoRequest.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+            startActivity(intent)
         }
-        mediaPlayer = MediaPlayer().apply {
-            setAudioStreamType(AudioManager.STREAM_MUSIC)
-            setDataSource(applicationContext, uri)
-            prepare()
-            start()
-        }
-    }
-
-    private fun runtimePermission() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            requestPermission()
-        }else{
-            viewModel.displayMusic(this)
-        }
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val pos = savedInstanceState.getInt("posicion")
-        mediaPlayer.seekTo(pos)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val pos = mediaPlayer.currentPosition
-        outState.putInt("posicion", pos)
     }
 
     override fun onResume() {
         super.onResume()
-        mediaPlayer.start()
         runtimePermission()
     }
 
-    override fun onStop() {
-        super.onStop()
-        mediaPlayer.seekTo(mediaPlayer.currentPosition)
+    private fun setObservers() {
+        viewModel.isPermission.observe(this){
+            binding.layoutPermissionNoRequest.isVisible = !it
+            binding.fragmentContainer.isVisible = it
+        }
     }
 
-    override fun onRestart() {
-        super.onRestart()
-        mediaPlayer.seekTo(mediaPlayer.currentPosition)
-    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaPlayer.seekTo(mediaPlayer.currentPosition)
-    }
-
-    private fun getDuration(uri: Uri): Long? {
-        val retriever =  MediaMetadataRetriever()
-        retriever.setDataSource(this, uri)
-        val time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
-        val timeMilliSec = time?.toLong()
-        retriever.release()
-        return timeMilliSec
+    private fun runtimePermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            viewModel.setPermission(false)
+            requestPermission()
+        }else{
+            viewModel.setPermission(true)
+        }
     }
 
 
     private fun requestPermission() {
         if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+            viewModel.setPermission(false)
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 777)
         }else{
             Toast.makeText(this, "Go to Settings and accept the permissions", Toast.LENGTH_SHORT).show()
@@ -132,9 +78,10 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if(requestCode == 777){
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                viewModel.displayMusic(context = this)
+                viewModel.setPermission(true)
             }else{
                 Toast.makeText(this, "Go to Settings and accept the permissions", Toast.LENGTH_SHORT).show()
+                viewModel.setPermission(false)
             }
         }
     }
--- app/src/main/java/com/example/mp3x/ui/music/view/MusicFragment.kt
+++ app/src/main/java/com/example/mp3x/ui/music/view/MusicFragment.kt
@@ -1,60 +1,122 @@
 package com.example.mp3x.ui.music.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.AudioManager
+import android.media.MediaMetadataRetriever
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mp3x.R
+import com.example.mp3x.adapter.MusicAdapter
+import com.example.mp3x.databinding.FragmentMusicBinding
+import com.example.mp3x.model.Music
+import com.example.mp3x.provider.ProviderMusic
+import com.example.mp3x.ui.music.viewmodel.MusicViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MusicFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MusicFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val viewModel: MusicViewModel by viewModels()
+    private var _binding: FragmentMusicBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var adapter: MusicAdapter
+    private var mediaPlayer = MediaPlayer()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music, container, false)
+    ): View {
+        _binding = FragmentMusicBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MusicFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MusicFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecycler()
+        setObservers()
+        setListener()
+        viewModel.displayMusic(requireContext())
+    }
+
+    private fun setListener() {
+        binding.lavPlayPause.setOnClickListener {
+            handleStateMusic()
+        }
+        binding.lavFavorite.setOnClickListener {
+            binding.lavFavorite.playAnimation()
+        }
+    }
+
+    private fun handleStateMusic(){
+        if(mediaPlayer.isPlaying){
+            binding.lavPlayPause.frame = 0
+            mediaPlayer.seekTo(mediaPlayer.currentPosition)
+            mediaPlayer.pause()
+        }else{
+            binding.lavPlayPause.frame = 60
+            mediaPlayer.start()
+        }
+    }
+
+    private fun setObservers() {
+
+    }
+
+    private fun initRecycler() {
+        adapter = MusicAdapter(
+            listMusic = ProviderMusic.listMusic,
+            onClickListener = {music -> onItemSelected(music) }
+        )
+        val manager = LinearLayoutManager(context)
+        binding.recyclerView.layoutManager = manager
+        binding.recyclerView.adapter = adapter
+    }
+
+    private fun onItemSelected(item: Music) {
+        val uri = Uri.parse(item.uri.toString())
+        if(mediaPlayer.isPlaying){
+            mediaPlayer.release()
+        }
+        mediaPlayer = MediaPlayer().apply {
+            setAudioStreamType(AudioManager.STREAM_MUSIC)
+            setDataSource(requireContext(), uri)
+            prepare()
+            start()
+        }
+        configureToolsReproduce(item)
+    }
+
+    private fun configureToolsReproduce(item: Music) {
+        binding.tvTitleMusic.text = item.name
+        binding.tvAuthorMusic.text = item.author
+        val imageAlbum = getAlbumImage(item.path)
+        if(imageAlbum == null){
+            binding.ivAlbumSelected.setImageResource(R.drawable.unknown_album)
+        }else{
+            binding.ivAlbumSelected.setImageBitmap(imageAlbum)
+        }
+    }
+
+    private fun getAlbumImage(path: String): Bitmap? {
+        val mmr = MediaMetadataRetriever()
+        mmr.setDataSource(path)
+        val data = mmr.embeddedPicture
+        return if (data != null) BitmapFactory.decodeByteArray(data, 0, data.size) else null
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlayer.start()
     }
 }
